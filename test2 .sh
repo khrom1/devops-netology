7727e0e9bb62025fcf92cfeeb69a442c91ffa61b
Khromov_Mikhail <khrom1@yandex.ru>
2022-01-05 13:20:33 +0300

    9 0

--- test2 .sh
+++ test2 .sh
@@ -11,3 +11,31 @@ date >>test_ip.log
     done
 done
 
+
+#!/usr/bin/env bash
+IP=(192.168.0.1 173.194.222.113 87.250.250.24)
+timeout=1
+stat=0
+while [$stat == 0]
+do
+    for ip in ${IP[*]}
+    do
+        curl -Is --connect-timeout 1 $ip:80 >/dev/null
+        echo  date "       ERROR $ip "  status=$? >>2_test_ip.log
+    done
+done
+
+#!/usr/bin/env bash
+IP=(192.168.0.1 173.194.222.113 87.250.250.24)
+timeout=1
+stat=0
+while (($stat == 0))
+do
+    for ip in ${IP[*]}
+    do
+        curl -Is --connect-timeout 1 $ip:80 >/dev/null
+        echo $date "      ERROR $ip "  status=$? >>2_test_ip.log
+
+      fi
+   done
+done
